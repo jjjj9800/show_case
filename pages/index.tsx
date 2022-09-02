d435b6f5dc34b8c1704a0a258a65139eaa7011e9
Yuei Lee <jjjj9800@gmail.com>
2022-09-02 12:31:03 +0800

    [新增] example useReduces

---
+++ pages/index.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from 'next'
+import React, {useReducer} from "react";
+
+interface ITransformAction{
+  type: string,
+  payload: number
+}
+
+interface ITransformState{
+  position: {x:number, y:number, z:number}
+}
+
+function transformReducer(state: ITransformState, action:ITransformAction) {
+  const {type, payload} = action;
+  switch (type) {
+    case "position.x":
+      return {
+        ...state,
+        value: state.position.x = payload
+      }
+    case "position.y":
+      return {
+        ...state,
+        value: state.position.y = payload
+      }
+    case "position.z":
+      return {
+        ...state,
+        value: state.position.z = payload
+      }
+    default:
+      return state;
+  }
+}
+
+const Home: NextPage = () => {
+  const [state, dispatch] = useReducer(transformReducer, {position: {x:0, y:0, z:0}});
+
+  function onChangeValue(evt:React.ChangeEvent<HTMLInputElement>) {
+    const {name, value} = evt.currentTarget;
+    // 注意這邊從input來的值會是 string型別所以要把它轉成number型別
+    console.log(name, value)
+    dispatch({type:name, payload: +value});
+  }
+
+  return (
+    <div>
+      <p>Position</p>
+      <span>X:<input type={"number"} value={state.position.x} name={"position.x"} onChange={onChangeValue} /></span>
+      <span>Y:<input type={"number"} value={state.position.y} name={"position.y"} onChange={onChangeValue} /></span>
+      <span>Z:<input type={"number"} value={state.position.z} name={"position.z"} onChange={onChangeValue} /></span>
+    </div>
+  )
+}
+
+export default Home
